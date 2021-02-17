6ff134f4bd0c8fe3796a0059829fc2d23a918ea9
stadvar <stadvar@gmail.com>
2021-02-17 21:38:00 +0200

    Revert "made api contacts & routes"
    This reverts commit 0bfedb09febb88c47d5432135aa85e2dd77f1724.

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,64 +1,24 @@
-const express = require("express");
-const router = express.Router();
-const { nanoid } = require("nanoid");
-const {
-  listContacts,
-  getContactById,
-  updateContact,
-  removeContact,
-  addContact,
-} = require("../../model/index");
+const express = require('express')
+const router = express.Router()
 
-router.get("/", async (req, res, next) => {
-  try {
-    const data = await listContacts();
-    res.json(data);
-  } catch (e) {
-    next(e);
-  }
-});
+router.get('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const data = await getContactById(req.params.contactId);
-    if (data.message) {
-      res.status(404).json(data);
-    } else {
-      res.json(data);
-    }
-  } catch (e) {
-    next(e);
-  }
-});
+router.get('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.post("/", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-  const data = await addContact(nanoid(4), name, email, phone);
-  if (data.message) return res.status(400).json(data);
-  return res.status(201).json(data);
-});
+router.post('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const data = await removeContact(req.params.contactId);
-    if (data.message === "Not found") return res.status(404).json(data);
-    if (data.message === "contact deleted") return res.json(data);
-  } catch (e) {
-    next(e);
-  }
-});
+router.delete('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.patch("/:contactId", async (req, res, next) => {
-  try {
-    const data = await updateContact(req.params.contactId, req.body);
-    if (data.message === "Not found") return res.status(404).json(data);
-    if (data.message === "missing fields") {
-      return res.status(400).json(data);
-    }
-    return res.status(200).json(data);
-  } catch (e) {
-    next(e);
-  }
-});
+router.patch('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-module.exports = router;
+module.exports = router
