11a43aae5d9a8fdf513e88727a4b4edd85df4619
stadvar <stadvar@gmail.com>
2021-02-17 21:50:06 +0200

    made contacts api & routes

--- model/index.js
+++ model/index.js
@@ -1,15 +1,91 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.resolve("model/contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data.toString());
+    return contacts;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find(({ id }) => contactId === id.toString());
+  if (!contact) return { message: "Not found" };
+  return contact;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  let isContactIdMatch = false;
+  const contacts = await listContacts();
+  const list = contacts.filter(({ id }) => {
+    if (contactId === id.toString()) {
+      isContactIdMatch = true;
+      return false;
+    } else {
+      return true;
+    }
+  });
 
-const addContact = async (body) => {}
+  if (!isContactIdMatch) return { message: "Not found" };
 
-const updateContact = async (contactId, body) => {}
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+    return { message: "contact deleted" };
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const addContact = async (id, name, email, phone) => {
+  if (!id || !name || !email || !phone) {
+    return { message: "missing required name field" };
+  }
+  const contacts = await listContacts();
+  const contact = { id, name, email, phone };
+  try {
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify([...contacts, contact], null, 2)
+    );
+    return contact;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const updateContact = async (contactId, data) => {
+  let isContactIdMatch = false;
+  let contactIndexInArr = -1;
+  const contacts = await listContacts();
+  const list = contacts.filter((el, i) => {
+    if (contactId === el.id.toString()) {
+      isContactIdMatch = true;
+      contactIndexInArr = i;
+      return false;
+    } else {
+      return true;
+    }
+  });
+  if (!isContactIdMatch) return { message: "Not found" };
+  if (Object.keys(data).length === 0) return { message: "missing fields" };
+
+  const updatedContact = { ...contacts[contactIndexInArr], ...data };
+
+  try {
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify([...list, updatedContact], null, 2)
+    );
+    return updatedContact;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +93,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
